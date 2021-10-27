d6ed3e0f3892a13fe84638dc8a90298a80d93b5e
ihrimon <ihrimon147@gmail.com>
2021-10-28 00:47:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+// initial 5 steps for api server
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors');
+// environment user require
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// user: geniuscar
+// password: aK4arL1dWBMrJa6N
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zaiok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('Connect to database')
+        const database = client.db('carMechanic');
+        const servicesCollection = database.collection('services');
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // GET Single Service
+        app.get('services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('Getting specific service', id);
+            const query = { _id: ObjectId(id) }
+            const service = await servicesCollection.findOne(query);;
+            res.json(service);
+        });
+        // POST API 
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+            // const service = {
+            //     "name": "ENGINE DIAGNOSTIC",
+            //     "price": "300",
+            //     "description": "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.",
+            //     "img": "https://i.ibb.co/dGDkr4v/1.jpg"
+            // }
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            // res.send('post hitted')
+            res.json(result);
+        });
+
+        // DELETE API
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Running genius server on the ui display');
+})
+
+app.listen(port, () => {
+    console.log('Running Genius Server on Port ', port);
+});
+
